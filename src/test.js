1eef7dc9077c235f9b981a876dadb31e322f7820
vladsergy <drylb924@gmail.com>
2020-12-30 00:12:59 +0200

    magic of valueOf()

---
+++ src/test.js
@@ -0,0 +1,10 @@
+// console.log({}.toString());
+
+// console.log(Object.prototype.toString.call(Symbol('key', 1)));
+// console.log(Object.prototype.toString.call([]));
+
+const myArray = [1, 2, 3, 4, 5];
+const temp = myArray.toString();
+// console.log(+temp);
+console.log(typeof temp);
+console.log(temp instanceof Number);
